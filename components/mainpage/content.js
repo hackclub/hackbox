aa0670b57f80c2b6e4f133b646cf557eaf4448e8
bajpai244 <bajpaiharsh244@gmail.com>
2020-08-30 21:57:25 +0530

    completed what is included

--- components/mainpage/content.js
+++ components/mainpage/content.js
@@ -19,16 +19,17 @@ export default () => (
     </Text>
     <Grid columns={[2, 2, 3, 4]} sx={{ pt: [4] }}>
       {arr.map((ele) => (
-        <ContentElement text={ele.text} bg={ele.bg} />
+        <ContentElement text={ele.text} bg={ele.bg} background={ele.bggrad} />
       ))}
     </Grid>
   </Box>
 )
 
-const ContentElement = ({ text, bg }) => (
+const ContentElement = ({ text, bg, background }) => (
   <Flex
     sx={{
       bg: bg ? bg : 'steel',
+      background: background ? background : null,
       color: 'white',
       height: [150],
       borderRadius: [3, 5],
@@ -53,12 +54,44 @@ const arr1 = [
 ]
 
 const arr = [
-  { text: '#Programming', bg: 'red' },
-  { text: '#Courses', bg: 'purple' },
-  { text: '#Books', bg: 'yellow' },
-  { text: '#Khan Academy', bg: 'orange' },
-  { text: '#Music', bg: 'green' },
-  { text: '#Graphics', bg: 'steel' },
-  { text: '#Workshops', bg: 'blue' },
-  { text: '#Operating System', bg: 'cyan' },
+  {
+    text: '#Programming',
+    bg: 'red',
+    bggrad: 'linear-gradient(180deg, #FF1334 7.38%, #C90C26 100%)',
+  },
+  {
+    text: '#Courses',
+    bg: 'purple',
+    bggrad: 'linear-gradient(180deg, #B618F8 0%, #8406B8 100%)',
+  },
+  {
+    text: '#Books',
+    bg: 'yellow',
+    bggrad: 'linear-gradient(180deg, #FFDB48 0%, #C8A000 100%)',
+  },
+  {
+    text: '#Khan Academy',
+    bg: 'orange',
+    bggrad: 'linear-gradient(180deg, #FF9C53 0%, #EB6400 100%)',
+  },
+  {
+    text: '#Music',
+    bg: 'green',
+    bggrad: 'linear-gradient(180deg, #16EDAE 0%, #16BA8A 100%)',
+  },
+  {
+    text: '#Graphics',
+    bg: 'steel',
+    bggrad: 'linear-gradient(180deg, #3C5F89 0%, #212D3C 100%)',
+  },
+  {
+    text: '#Workshops',
+    bg: 'blue',
+    bggrad: 'linear-gradient(180deg, #4A9BDE 0%, #145891 100%)',
+  },
+  {
+    text: '#Operating System',
+    bg: 'cyan',
+    bggrad: ' linear-gradient(180deg, #38C1EA 0%, #06789A 100%)',
+  },
 ]
